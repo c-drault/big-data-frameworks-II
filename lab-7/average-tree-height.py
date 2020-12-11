7ec536d54a150a691ec1c8d32bc08150da3c839c
Corentin DRAULT <corentin.drault@orange.fr>
2020-12-11 16:44:31 +0100

    Update average-tree-height.py

--- lab-7/average-tree-height.py
+++ lab-7/average-tree-height.py
@@ -8,7 +8,7 @@ sc = SparkContext(conf=conf)
 
 file = sc.textFile("hdfs:/user/cdrault/trees.csv")
 trees = file.map(lambda line: Tree(line))
-heights = trees.map(lambda tree: tree.hauteur())
+heights = trees.map(lambda tree: tree.height())
 good_heights = heights.filter(lambda val: val is not None)
 
 average = good_heights.sum()/good_heights.count()
