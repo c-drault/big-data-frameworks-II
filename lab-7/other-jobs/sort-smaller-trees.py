ea53ade2926c5baf2146fafb6d9bb8e93f7d6a7d
Corentin DRAULT <corentin.drault@orange.fr>
2020-12-11 17:38:58 +0100

    Create sort-smaller-trees.py

---
+++ lab-7/other-jobs/sort-smaller-trees.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+from pyspark import SparkConf, SparkContext
+from tree import Tree
+
+appName = "sortBySmallerTrees"
+conf = SparkConf().setAppName(appName)
+sc = SparkContext(conf=conf)
+
+file = sc.textFile("hdfs:/user/cdrault/trees.csv")
+trees = file.map(lambda line: Tree(line))
+
+species = trees.map(lambda tree: (tree.height(), tree.specie()) )
+species_f = species.filter(lambda (h, s) : h is not None and s is not None)
+species_sort = species_f.sortByKey()
+print "Species : %s" % (species_sort.collect())
