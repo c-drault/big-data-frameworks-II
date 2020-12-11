93b08bda0800c8ff47ee184c9f58b911946e33d4
Corentin DRAULT <corentin.drault@orange.fr>
2020-12-11 17:49:26 +0100

    Update tree.py

--- lab-7/tree.py
+++ lab-7/tree.py
@@ -12,15 +12,22 @@ class Tree(object):
         return self.data[2]
 
     def height(self):
-        if self.champs[6] == u'HAUTEUR': return None
+        if self.data[6] == u'HAUTEUR': return None
         try:
             return float(self.data[6])
         except:
             return None
     
     def year(self):
-        if self.champs[5] == u'ANNEE PLANTATION': return None
+        if self.data[5] == u'ANNEE PLANTATION': return None
         try:
-            return int(self.champs[5])
+            return int(self.data[5])
+        except:
+            return None
+        
+    def district(self):
+        if self.data[1] == u'ARRONDISSEMENT': return None
+        try:
+            return int(self.data[1])
         except:
             return None
