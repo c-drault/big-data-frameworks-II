03bf62c2fbd01c528c2d4455f4c2e623dffed824
Corentin DRAULT <corentin.drault@orange.fr>
2020-12-11 17:16:47 +0100

    Update different-species-trees.py

--- lab-7/other-jobs/different-species-trees.py
+++ lab-7/other-jobs/different-species-trees.py
@@ -11,6 +11,6 @@ file = sc.textFile("hdfs:/user/cdrault/trees.csv")
 
 trees = file.map(lambda line: Tree(line))
 species = trees.map(lambda tree: tree.specie())
-species_f = trees.filter(lambda s: s is not None)
+species_f = species.filter(lambda s: s is not None)
 species_d = species_f.distinct()
 print "Species : %s" % (species_d.collect())
