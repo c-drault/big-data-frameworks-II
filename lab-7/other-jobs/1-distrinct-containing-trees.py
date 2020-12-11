81250f1e551e96bc50c439afe3c8b78f80325605
Corentin DRAULT <corentin.drault@orange.fr>
2020-12-11 19:33:18 +0100

    Update and rename distinct-containing-trees.py to 1-distrinct-containing-trees.py

--- lab-7/other-jobs/1-distrinct-containing-trees.py
+++ lab-7/other-jobs/1-distrinct-containing-trees.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python
-
 from pyspark import SparkConf, SparkContext
 from tree import Tree
 
-appName = "distinctContainingTrees"
+appName = "distrinctContainingtrees"
 conf = SparkConf().setAppName(appName)
 sc = SparkContext(conf=conf)
 
 file = sc.textFile("hdfs:/user/cdrault/trees.csv")
-
 trees = file.map(lambda line: Tree(line))
 
 districts = trees.map(lambda tree: tree.district())
-districts_filtered = districts.filter(lambda height: height is not None)
-districts_distinct = districts_filtered.distinct()
-coll = districts_distinct.collect()
-print "District containing trees : %s" % (coll)
+coll = districts.collect()
+print "Districts : %s" % (coll)
